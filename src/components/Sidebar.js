d2e12be1b19b51fd4a689ae8393c57c652b9850e
prateekvarma <prtk.varma@gmail.com>
2023-08-03 15:07:37 -0600

    Open sidebar logic

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,13 +9,12 @@ import CartButtons from "./CartButtons";
 import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
-  const data = useProductsContext();
-  console.log(data);
+  const { isSidebarOpen, closeSidebar } = useProductsContext();
 
-  const isOpen = true; //temporary
+  const isOpen = false; //temporary
   return (
     <SidebarContainer>
-      <aside className={`${isOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
         <div className="sidebar-header">
           <img src={logo} className="logo" alt="comfy sloth" />
           <button className="close-btn" type="button">
