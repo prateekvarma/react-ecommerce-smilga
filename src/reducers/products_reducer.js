7ea7580ccd4f9bf3b7c5f9addb9a35c8bc7c15e7
prateekvarma <prtk.varma@gmail.com>
2023-08-10 11:26:44 -0600

    Context and reducer to fetch all products

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -16,7 +16,20 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false };
   }
-  return state;
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    );
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_products,
+    };
+  }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
