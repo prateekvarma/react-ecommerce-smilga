a4401a3676459536ea0640b2da302bbe42064f5d
prateekvarma <prtk.varma@gmail.com>
2023-09-17 15:34:25 -0600

    List view for products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import { useFilterContext } from '../context/filter_context'
-import GridView from './GridView'
-import ListView from './ListView'
+import React from "react";
+import { useFilterContext } from "../context/filter_context";
+import GridView from "./GridView";
+import ListView from "./ListView";
 
 const ProductList = () => {
-  const { filtered_products: products } = useFilterContext()
+  const { filtered_products: products, grid_view } = useFilterContext();
 
-  return <GridView products={products} />
-}
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: "none" }}>
+        Sorry, no products match your search...
+      </h5>
+    );
+  }
 
-export default ProductList
+  if (grid_view === false) {
+    return <ListView products={products} />;
+  }
+
+  return <GridView products={products} />;
+};
+
+export default ProductList;
