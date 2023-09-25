7277f2d0a85287d18e72f443b91db092ce3c5a88
prateekvarma <prtk.varma@gmail.com>
2023-09-25 12:17:10 -0600

    Finished sort functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -28,20 +28,24 @@ const filter_reducer = (state, action) => {
   }
   if (action.type === SORT_PRODUCTS) {
     const { sort, filtered_products } = state;
-    let tempProducts = [];
+    let tempProducts = [...filtered_products];
     if (sort === "price-lowest") {
-      console.log("price-lowest");
+      tempProducts = tempProducts.sort((a, b) => a.price - b.price);
     }
     if (sort === "price-highest") {
-      console.log("price-highest");
+      tempProducts = tempProducts.sort((a, b) => b.price - a.price);
     }
     if (sort === "name-a") {
-      console.log("name-a");
+      tempProducts = tempProducts.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
     }
     if (sort === "name-z") {
-      console.log("name-z");
+      tempProducts = tempProducts.sort((a, b) => {
+        return b.name.localeCompare(a.name);
+      });
     }
-    return { ...state };
+    return { ...state, filtered_products: tempProducts };
   }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
